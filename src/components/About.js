c6a7ae4c0f6881572a63b3d9ea164b3b5a1075b7
Amogh Manral <amogh.manral@duke.edu>
2025-07-07 14:10:55 +0200

    gh actions workflow

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
                 <div className="section-header">
                     <h2 className="section-title">about</h2>
                     <p className="section-subtitle">
-                        I build cool things. Currently working as a SWE intern. 
+                        I build interesting things. Currently working as a SWE intern at SLB. When I'm not busy with that, I watch Formula 1, go rock climbing, or play piano.
 
                     </p>
                 </div>
